73969cc9fade121fcc0d904b40a23cb8b198b77a
who <nadezhda.bn.02@mail.ru>
2022-11-13 21:00:33 +0300

    lab9

---
+++ lab9 WebDriver/BringItOn .cs
@@ -0,0 +1,54 @@
+
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using static lab9_WebDriver.Tests;
+
+
+namespace lab9_WebDriver;
+
+
+public class BringItOn : TestsClass
+{
+    protected override string? Url =>  "https://pastebin.com";
+
+    private string nameValue = "how to gain dominance among developers";
+    private string codeValue = "git config --global user.name  'New Sheriff in Town' \ngit reset $(git commit-tree HEAD^{tree} -m 'Legacy code')\ngit push origin master --force";
+
+
+    [Test]
+    public void Test1()
+    {
+       
+        var codeInput = findElement(By.Id("postform-text"));
+        codeInput.SendKeys(codeValue);
+
+        findElement(By.Id("select2-postform-format-container")).Click();
+        findElement(By.XPath("//li[text()='Bash']")).Click();
+
+        findElement(By.Id("select2-postform-expiration-container")).Click();
+        findElement(By.XPath("//li[text()='10 Minutes']")).Click();
+
+        var nameInput = findElement(By.Id("postform-name"));
+        nameInput.SendKeys(nameValue);
+        findElement(By.XPath("//button[@class='btn -big']")).Click();
+        Thread.Sleep(2000);
+
+        var title = driver.Title;
+        var nameText = findElement("//h1").Text; //equals with nameValue
+        var codeText = findElement("//ol").Text;
+        var selectedFormat = findElement("//a[@class='btn -small h_800']").Text;
+
+
+        Assert.IsTrue(title == nameValue && codeText.Contains(codeValue) && selectedFormat == "Bash");
+
+
+
+        //Assert.IsTrue(currentUrl != Url);
+        
+
+    }
+
+}
