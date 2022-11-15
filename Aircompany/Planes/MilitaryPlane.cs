99e41e8d09b15b697e21e7771a42326dfbbe139e
who <nadezhda.bn.02@mail.ru>
2022-11-15 22:17:27 +0300

    add new changes

---
+++ Aircompany/Planes/MilitaryPlane.cs
@@ -0,0 +1,39 @@
+﻿using Aircompany.Models;
+
+namespace Aircompany.Planes
+{
+    public class MilitaryPlane : Plane
+    {
+        private readonly MilitaryType _type;
+        public MilitaryType Type { get => _type; }
+
+        public MilitaryPlane(string model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, MilitaryType type)
+            : base(model, maxSpeed, maxFlightDistance, maxLoadCapacity)
+        {
+            _type = type;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var plane = obj as MilitaryPlane;
+            return plane != null &&
+                   base.Equals(obj) &&
+                   _type == plane._type;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 1701194404;
+            hashCode = hashCode * -1521134295 + base.GetHashCode();
+            hashCode = hashCode * -1521134295 + _type.GetHashCode();
+            return hashCode;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString().Replace("}",
+                    ", type=" + _type +
+                    '}');
+        }        
+    }
+}
